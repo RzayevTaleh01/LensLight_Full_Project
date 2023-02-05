2c6cc7347e99c55e61a4cdfc9b25d3b034b514e9
rzayevtaleh01 <taleh.rzayev.i@asoiu.edu.az>
2023-02-05 21:09:32 +0400

    Separation Of Concerns, MVC

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js"
 
 dotenv.config();
 //connetcion to the db
@@ -15,14 +16,9 @@ app.set("view engine", "ejs")
 //static files middleware
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index");
-})
+//routes
 
-app.get("/about", (req, res) => {
-    res.render("about");
-})
-
+app.use("/", pageRoute );
 
 app.listen(port, () => {
     console.log(`Application running on port: ${port}`);
