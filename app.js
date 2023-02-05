569809b2914b42e97932c9e01e9c4e669d3ce4a9
rzayevtaleh01 <taleh.rzayev.i@asoiu.edu.az>
2023-02-05 18:59:24 +0400

    added Template Engine, Views and Partials

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+//ejs template engine
+app.set("view engine","ejs")
+
+//static files middleware
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
-    res.send("Index 2");
+    res.render("index");
 })
+
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
+
+
 
 app.listen(port,()=>{
     console.log(`Application running on port: ${port}`);
