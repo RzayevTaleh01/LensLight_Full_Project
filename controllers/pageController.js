2f1d9034cbe06d53ac947af89f187e17f2e3b0a1
rzayevtaleh01 <taleh.rzayev.i@asoiu.edu.az>
2023-02-06 14:39:56 +0400

    Render Function, List Data, Added Active Link - 07

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,8 +1,12 @@
 const getIndexPage = (req,res) =>{
-    res.render("index")    
+    res.render("index",{
+        link: "index"
+    })    
 }
 
 const getAboutPage = (req,res) =>{
-    res.render("about")    
+    res.render("about",{
+        link: "about"
+    })    
 }
 export {getIndexPage,getAboutPage};
--- controllers/photoController.js
+++ controllers/photoController.js
@@ -20,10 +20,10 @@ const createPhoto = async (req, res) => {
 const getAllPhotos = async (req, res) => {
     try {
         const photos = await Photo.find({})
-        res.status(200).json({
-            succeded: true,
-            photos
-        })
+        res.status(200).render("photos",{
+            photos,
+            link: "photos"
+        });
 
     } catch (error) {
         res.status(500).json({
