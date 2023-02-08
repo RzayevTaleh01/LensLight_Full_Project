14285c0f2c67969b6516d12b800c9a4e96f1ecce
Taleh Rzayev <taleh.rzayev.i@asoiu.edu.az>
2023-02-09 01:20:06 +0400

    Revert "Added Validation,Form Editing, Error Catching - 14"
    This reverts commit eeaa1bc1e41748ea9e8bb05ab08af9564d5e43b0.

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,22 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt"
-import validator from "validator";
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
     username: {
         type: String,
-        required: [true, "Username area is required!"],
-        lowarcase: true,
-        validate: [validator.isAlphanumeric,"Only Alphanumeric characters!"]
+        required: true,
+        unique: true
     },
     email: {
         type: String,
-        required: [true, "Email area is required!"],
-        unique: true,
-        validate: [validator.isEmail, "Valid email is required!"]
+        required: true,
+        unique: true
     },
     password: {
         type: String,
-        required: [true, "Password area is required!"],
-        minLength: [4, "At least 4 characters"]
+        required: true,
     }
 }, {
     timestamps: true
