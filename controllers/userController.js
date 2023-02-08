eeaa1bc1e41748ea9e8bb05ab08af9564d5e43b0
rzayevtaleh01 <taleh.rzayev.i@asoiu.edu.az>
2023-02-08 16:29:35 +0400

    Added Validation,Form Editing, Error Catching - 14

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,77 +1,81 @@
-import User from "../models/userModel.js";
-import bcyrpt from "bcrypt"
-import jwt from "jsonwebtoken";
+import User from '../models/userModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const createUser = async (req, res) => {
     try {
-        const user = await User.create(req.body)
-        res.redirect("/login")
+        const user = await User.create(req.body);
+        res.status(201).json({ user: user._id });
     } catch (error) {
-        res.status(500).json({
-            succeded: false,
-            error
-        });
+        console.log('ERROR', error);
+
+        let errorsMessages = {};
+
+        if (error.code === 11000) {
+            errorsMessages.email = 'The Email is already registered!';
+        }
+
+        if (error.name === 'ValidationError') {
+            Object.keys(error.errors).forEach((key) => {
+                errorsMessages[key] = error.errors[key].message;
+            });
+        }
+
+        console.log('ErrorsMessages:::', errorsMessages);
+
+        res.status(400).json(errorsMessages);
     }
-}
+};
 
 const loginUser = async (req, res) => {
-
     try {
-        //requstən username və password alırıq
-        const { username, password } = req.body
+        const { username, password } = req.body;
 
-        //bazada bu adda user varmı deyə yoxlayırıq
-        const user = await User.findOne({ username })
+        const user = await User.findOne({ username });
+
         let same = false;
+
         if (user) {
-            //əgər user varsa daxil edilmiş parol ilə bazadakı userin parolunu yoxlayırıq
-            same = await bcyrpt.compare(password, user.password)
-        }
-        else {
-            //user yoxdursa 401 erroru
+            same = await bcrypt.compare(password, user.password);
+        } else {
             return res.status(401).json({
                 succeded: false,
-                error: "There is no such user!"
+                error: 'There is no such user',
             });
         }
+
         if (same) {
             const token = createToken(user._id);
-            //tokenin cookieyə əlavə edilməsi
-            res.cookie("jsonwebtoken",token,{
-                httpOnly:true,
-                maxAge: 1000*60*6*24, //cookienin silinmə müddəti
-
+            res.cookie('jwt', token, {
+                httpOnly: true,
+                maxAge: 1000 * 60 * 60 * 24,
             });
-            res.redirect("/users/dashboard") // əməliyyat düzdürsə yönləndiriləcəyi yer
-        }
-        else {
-            //parol səhvdirsə 401 erroru
+
+            res.redirect('/users/dashboard');
+        } else {
             res.status(401).json({
                 succeded: false,
-                error: "Password are not matched!"
+                error: 'Paswords are not matched',
             });
         }
-
     } catch (error) {
         res.status(500).json({
             succeded: false,
-            error
+            error,
         });
     }
-
-}
+};
 
 const createToken = (userId) => {
     return jwt.sign({ userId }, process.env.JWT_SECRET, {
-      expiresIn: '1d',
+        expiresIn: '1d',
     });
-  };
+};
 
-const getDashboardPage = (req,res)=>{
-    res.render("dashboard",{
-        link: 'dashboard'
-    })
-}
+const getDashboardPage = (req, res) => {
+    res.render('dashboard', {
+        link: 'dashboard',
+    });
+};
 
-export { createUser, loginUser,getDashboardPage };
-
+export { createUser, loginUser, getDashboardPage };
